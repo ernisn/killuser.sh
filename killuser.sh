1eae30a51226d258eaad5b59693ddae8ff8a2337
ernie <ivessty@gmail.com>
2019-05-21 16:04:58 +0800

    完善了在线用户数量的判定

--- killuser.sh
+++ killuser.sh
@@ -13,29 +13,41 @@ CheckMe(){
 
 #在线用户总数
 CheckAllUsers(){
-    users_total=`ps -ef | grep sshd | grep @pts | wc -l`
+#   users_total=`ps -ef | grep sshd | grep @pts | wc -l`
+#   这个方法在结束后不会立马更新用户数，先不用了
+    users_folder_num=`ls /dev/pts/ | wc -l`
+	users_total=$(($users_folder_num-1))
 }
 
 #结束用户及其进程
 KillUser(){
 	if  [ ! -n "$kill_num" ] ; then
-		echo -e "${red}输入错误${none}，请确认输入为整数。" 
+		echo -e "${red}输入错误${none}，请确认输入为整数" 
 		exit 1
     else
         case "$kill_num" in 
             [0-9]*)  
-			[[ $kill_num > $(($users_total-1)) ]] && echo -e "${red}输入错误${none}，该用户不存在。" && exit 1
+#    		kill_folder="/dev/pts/""$kill_num"
+#			[ ! -d "$kill_folder" ] && echo -e "${red}输入错误${none}，该用户不存在。" && exit 1
+#           输入合法性之后再判断吧，先去掉
 			[[ $kill_num = $user_num ]] && echo -e "${red}你选了自己${none}，请不要作死。" && exit 1
 			pkill -kill -t "pts/""${kill_num}"
-			if [ ! -d "/dev/pts/${kill_num}" ]; then
-                echo -e "已结束 ${green}${kill_num}${none} 号用户及其进程。" 
-	            echo -e "剩余用户数: ${green}$(($users_total-1))${none}" 
-			else
-			    echo -e "${red}结束用户失败。${none}" 
-            fi
+			echo -e "已结束 ${green}${kill_num}${none} 号用户及其进程。" 
+		    CheckAllUsers
+	        echo -e "剩余用户数: ${green}${users_total}${none}" 
+#			if [ ! -d "/dev/pts/""${kill_num}" ]; then
+#                echo -e "已结束 ${green}${kill_num}${none} 号用户及其进程。" 
+#				CheckAllUsers
+#	            echo -e "剩余用户数: ${green}${users_total}${none}" 
+#			else
+#			    echo -e "${red}结束用户失败。${none}" 
+#				CheckAllUsers
+#	            echo -e "剩余用户数: ${green}${users_total}${none}" 
+#           fi
+#           任务完成状态判断先不加了
             ;; 
             *)
-          	echo -e "${red}输入错误${none}，请确认输入为整数。" 
+          	echo -e "${red}输入错误${none}，请确认输入为整数" 
            	exit 1
             ;; 
         esac 
@@ -54,7 +66,7 @@ DoKill(){
 		echo -e "当前系统共有 ${green}${users_total}${none} 在线用户,连接信息如下: "
 		w -s -h
 		echo -e "其中当前终端(自己)为 ${green}${user_num}${none} 号用户，请选择要结束会话的用户编号。"
-		read -e -p "输入数字进行选择: " kill_num
+		read -e -p "输入数值进行选择: " kill_num
         KillUser
 	fi
 }
